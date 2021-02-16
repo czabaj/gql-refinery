28ba7ea91d4e955d9676af83b5547451afed6fc9
Vaclav Grohling <grohling.v@clav.cz>
2021-02-16 05:23:42 +0100

    chore: No newline in printed TS enums

--- src/typeScript/enumPrinter.ts
+++ src/typeScript/enumPrinter.ts
@@ -8,4 +8,4 @@ ${values.map((v) => `  ${upperSnakeCase(v)} = "${v}",`).join(`\n`)}
 
 export const printEnums = (enums: Enums): string =>
   Array.from(enums.entries()).map(([name, values]) => printEnum(name, values))
-    .join(`\n\n`);
+    .join(`\n`);
